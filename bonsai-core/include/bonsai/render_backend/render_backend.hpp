4e0f33d4abffd09c734e8689b261ebe2a0bf90db
Tijmen Verhoef <tijmenmenno@gmail.com>
2025-11-26 12:28:34 +0100

    adds doccomments to render backend

--- bonsai-core/include/bonsai/render_backend/render_backend.hpp
+++ bonsai-core/include/bonsai/render_backend/render_backend.hpp
@@ -4,6 +4,7 @@
 
 #include "bonsai/core/platform.hpp"
 
+/// @brief The RenderBackend wraps a backend graphics API, providing a common interface for the engine to use.
 class RenderBackend
 {
 protected:
@@ -11,6 +12,9 @@ protected:
 public:
     virtual ~RenderBackend() = default;
 
+    /// @brief Create a render backend.
+    /// @param platform_surface Main surface to use for rendering, will be used to initialize the render backend.
+    /// @return A new render backend, or nullptr if no backend is active.
     static RenderBackend* create(PlatformSurface* platform_surface);
 };
 
